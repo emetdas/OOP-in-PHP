c250cd92a565d829aa2d284c9d79fe94583f6517
emetdas <emet@b2bleadstaff.com>
2021-02-04 11:15:47 +0600

    opps in php

---
+++ basic/function.php
@@ -0,0 +1,9 @@
+<?php
+$con = new mysqli("localhost","root","","signup");
+if ($con->connect_error) {
+    echo "Data Dase not selected";
+}
+?>
+
+
+
---
+++ basic/index.php
@@ -0,0 +1,22 @@
+<?php
+
+class emet{ // class
+public $a,$b,$c; // propitis
+function sum(){ // method
+   $this->c = $this->a + $this->b;
+   return $this->c;
+}
+// constroctor function 
+// destroctor function 
+
+function sub(){
+   $this->c = $this->a - $this->b;
+   return $this->c;
+}
+}
+$sum = new emet(); // object
+
+$sum->a = 80;
+$sum->b = 40;
+echo $sum->sum()."<br>"; 
+echo $sum->sub();
