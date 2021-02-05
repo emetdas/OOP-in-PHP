4a21ad2aaf3d0095eaa67d1f48c31f299d0b5325
emetdas <emet@b2bleadstaff.com>
2021-02-05 20:11:15 +0600

    class and proprites learing

---
+++ basic/class.php
@@ -0,0 +1,15 @@
+<?php
+class emet{
+
+    public $name,$skill;
+    function info(){
+        $this->info = $this->name . $this->skill;
+        return $this->info;
+    }
+}
+$ifo = new emet();
+$ifo->name = "Emet<br>";
+$ifo->skill = "Java,Php";
+echo $ifo->info();
+
+?>
